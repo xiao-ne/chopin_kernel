mvaisakh/kernel_xiaomi_cannon

drivers: mtk_nanohub: Silence backlight notification work logspam
This is spammed beyond my control.

Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

drivers/misc/mediatek/sensor/2.0/mtk_nanohub/mtk_nanohub.c
@@ -2074,7 +2074,7 @@ static void cabc_backlight_value_notification_work(struct work_struct *work)
 		backlightness_config_data[1] = device->cabc_backlight_value;
 		backlightness_config_data[2] = 0;
 		backlightness_config_data[3] = 0;
-		pr_info("cabc_backlight_value_notification_work [%d, %d, %d, %d]\n",
+		pr_debug("cabc_backlight_value_notification_work [%d, %d, %d, %d]\n",
 			backlightness_config_data[0], backlightness_config_data[1],backlightness_config_data[2], backlightness_config_data[3]);
 
 		length = sizeof(backlightness_config_data);

xiao-ne/chopin_kernel

Signed-off-by: Tkpointz <pointzztk@gmail.com>

drivers/misc/mediatek/sensor/2.0/mtk_nanohub/mtk_nanohub.c
@@ -2356,7 +2356,7 @@ static void cabc_backled_data_notification_work(struct work_struct *work)
 		backled_state_data[1] = device->cabc_backled_data;
 		backled_state_data[2] = 0;
 		backled_state_data[3] = 0;
-		pr_info("cabc_backlight_data_notification_work [%d, %d, %d, %d]\n",
+		pr_debug("cabc_backlight_data_notification_work [%d, %d, %d, %d]\n",
 			backled_state_data[0], backled_state_data[1],backled_state_data[2], backled_state_data[3]);
 
 		length = sizeof(backled_state_data);